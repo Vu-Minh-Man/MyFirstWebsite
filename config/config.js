03ae7cafadf812bfc3b52f0966281840cf493e0f
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-15 07:15:42 +0700

    change config.

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
+const serverUrl = "https://sleepy-ocean-50803.herokuapp.com";
+
 const config = {
-  host: process.env.NEXT_PUBLIC_HOST,
-  apiUrl: process.env.NEXT_PUBLIC_SERVER_HOST + "/api",
+  //host: process.env.NEXT_PUBLIC_HOST,
+  apiUrl: serverUrl + "/api",
 };
 
 export default config;
