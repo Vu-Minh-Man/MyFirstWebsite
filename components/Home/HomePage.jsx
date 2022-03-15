dd02c521bd9a3c3bf7f9c8ab94eb671ae32315dd
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-15 12:44:06 +0700

    Fix Issues.

--- components/Home/HomePage.jsx
+++ components/Home/HomePage.jsx
@@ -3,7 +3,6 @@ function HomePage() {
     <>
       <h1>My Homepage</h1>
       <p>by Vũ Minh Mẫn</p>
-      <p>{process.env.SERVER_URL}</p>
     </>
   );
 }
--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //const serverUrl = "https://sleepy-ocean-50803.herokuapp.com" || "http://localhost:8080";
 
-const serverUrl = process.env.SERVER_URL || "http://localhost:8080";
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8080";
 
 const config = {
   apiUrl: serverUrl + "/api",
